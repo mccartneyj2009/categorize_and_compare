eb70cd44a90f63849a60fc0117f4f4f3bca0b90d
James McCartney <48179143+mccartneyj2009@users.noreply.github.com>
2021-06-07 19:54:59 -0400

    handle exeptions from user input errors

--- pg_scrape.py
+++ pg_scrape.py
@@ -24,51 +24,67 @@ def working_loop():
         print('Directory already exists')
     else:
         mkdir(f'{site_name}')
-    differences = int(input('Maximum differences until new Type? '))
 
-    lower_index = int(input('First device in range? (Enter BACnet Address)'))
-    upper_index = int(input('Last device in range? (Enter BACnet Address)'))
+    error_message = 'Enter an integer value.'
+    while True:
+        try:
+            differences = int(input('Maximum differences until new Type? '))
+            break
+        except ValueError:
+            print(error_message)
+    while True:
+        try:
+            lower_index = int(input('First device in range? (Enter BACnet Address)'))
+            break
+        except ValueError:
+            print(error_message)
+    while True:
+        try:
+            upper_index = int(input('Last device in range? (Enter BACnet Address)'))
+            break
+        except ValueError:
+            print(error_message)
 
     programs = [pg for pg in get_program_info(f"Select DEV_ID, INSTANCE, Program_Code, Object_Name FROM OBJECT_V4_PG WHERE SITE_ID = '{site_name}' AND DEV_ID BETWEEN {lower_index} and {upper_index}")]
     print(programs)
     number_of_errors = 0
 
-    # for pg in programs:
-    #     dev_id, instance, pg_code, obj_name = pg[0], pg[1], pg[2], pg[3]
-    #     try:
-    #         obj_name = obj_name.replace('/', '_')
-    #         if os.path.isdir(f'{site_name}'):
-    #             with open(f'{site_name}/Device {dev_id} - PG{instance} - {obj_name}.txt', 'x') as file:
-    #                 file.write(pg_code)
-    #         else:
-    #             mkdir(f'{site_name}')
-    #             with open(f'{site_name}/Device {dev_id} - PG{instance} - {obj_name}.txt', 'x') as file:
-    #                 file.write(pg_code)
-    #     except:
-    #         number_of_errors += 1
-    #         e = sys.exc_info()[0]
-    #         with open(f'ODBC Errors Report - {site_name}.txt', 'a')as f:
-    #             f.write(f'{e} : {dev_id} : {obj_name}\n')
-    #
-    # # Get a list of files in directory
-    # pg_names = ['Main.txt', 'Mode Program.txt', 'Status Program.txt', 'Supply Damper Control Program.txt',
-    #             'Exhaust Damper Control Program.txt', 'Damper Mode Program.txt', 'Heating Program.txt',
-    #             'Startup Program.txt', 'Calculations PG.txt', 'Stat_Display Program.txt', 'Blank Display Program.txt',
-    #             'BDE Extract PG.txt', 'Setpoints PG.txt', 'Damper Control Program.txt', 'Reheat Program.txt',
-    #             'Discharge Air PG.txt']
-    #
-    # contents_of_directory = os.listdir(f'{site_name}')
-    # for file in contents_of_directory:
-    #     file_strings = file.split(sep=' - ')
-    #     program_name = file_strings[2]
-    #     for pg_name in pg_names:
-    #         if program_name == pg_name:
-    #             if os.path.isdir(f'{site_name}/{pg_name} PGs'):
-    #                 os.rename(f'{site_name}/{file}', f'{site_name}/{pg_name} PGs/{file}')
-    #             else:
-    #                 mkdir(f'{site_name}/{pg_name} PGs')
-    #                 os.rename(f'{site_name}/{file}', f'{site_name}/{pg_name} PGs/{file}')
-    # create_file_types(site_name, differences)
-    # create_report(site_name)
-    #
-    # return 0
+    for pg in programs:
+        dev_id, instance, pg_code, obj_name = pg[0], pg[1], pg[2], pg[3]
+        try:
+            obj_name = obj_name.replace('/', '_')
+            if os.path.isdir(f'{site_name}'):
+                with open(f'{site_name}/Device {dev_id} - PG{instance} - {obj_name}.txt', 'x') as file:
+                    file.write(pg_code)
+            else:
+                mkdir(f'{site_name}')
+                with open(f'{site_name}/Device {dev_id} - PG{instance} - {obj_name}.txt', 'x') as file:
+                    file.write(pg_code)
+        except:
+            number_of_errors += 1
+            e = sys.exc_info()[0]
+            with open(f'ODBC Errors Report - {site_name}.txt', 'a')as f:
+                f.write(f'{e} : {dev_id} : {obj_name}\n')
+
+    # Get a list of files in directory
+    pg_names = ['Main.txt', 'Mode Program.txt', 'Status Program.txt', 'Supply Damper Control Program.txt',
+                'Exhaust Damper Control Program.txt', 'Damper Mode Program.txt', 'Heating Program.txt',
+                'Startup Program.txt', 'Calculations PG.txt', 'Stat_Display Program.txt', 'Blank Display Program.txt',
+                'BDE Extract PG.txt', 'Setpoints PG.txt', 'Damper Control Program.txt', 'Reheat Program.txt',
+                'Discharge Air PG.txt']
+
+    contents_of_directory = os.listdir(f'{site_name}')
+    for file in contents_of_directory:
+        file_strings = file.split(sep=' - ')
+        program_name = file_strings[2]
+        for pg_name in pg_names:
+            if program_name == pg_name:
+                if os.path.isdir(f'{site_name}/{pg_name} PGs'):
+                    os.rename(f'{site_name}/{file}', f'{site_name}/{pg_name} PGs/{file}')
+                else:
+                    mkdir(f'{site_name}/{pg_name} PGs')
+                    os.rename(f'{site_name}/{file}', f'{site_name}/{pg_name} PGs/{file}')
+    create_file_types(site_name, differences)
+    create_report(site_name)
+
+    return 0
